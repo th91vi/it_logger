64f7b8d80b94edd873627d0d6ef23f48f59f7e39
Thiago Vieira <th91v@outlook.com>
2020-09-04 18:56:02 -0300

    feat: SearchBar and Logs components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React, { useEffect } from 'react';
+import SearchBar from './components/layout/SearchBar'
+import Logs from './components/logs/Logs'
+
 import 'materialize-css/dist/css/materialize.min.css'
 import M from 'materialize-css/dist/js/materialize.min.js'
 import './App.css';
@@ -10,7 +13,12 @@ const App = () => {
   })
   return (
     <div className="App">
-      My App
+      <>
+        <SearchBar />
+        <div className='container'>
+          <Logs />
+        </div>
+      </>
     </div>
   );
 }
