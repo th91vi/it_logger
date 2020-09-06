ae6eddd091ca7e453075ceaa552c498735b54b4c
Thiago Vieira <th91v@outlook.com>
2020-09-06 17:24:40 -0300

    feat: added redux provider, initial redux logic

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import AddLogModal from './components/logs/AddLogModal'
 import EditLogModal from './components/logs/EditLogModal'
 import AddTechModal from './components/techs/AddTechModal'
 import TechListModal from './components/techs/TechListModal'
+import { Provider } from 'react-redux'
+import store from './store'
 
 import 'materialize-css/dist/css/materialize.min.css'
 import M from 'materialize-css/dist/js/materialize.min.js'
@@ -17,7 +19,7 @@ const App = () => {
     M.AutoInit();
   })
   return (
-    <div className="App">
+    <Provider store={store}>
       <>
         <SearchBar />
         <div className='container'>
@@ -29,7 +31,7 @@ const App = () => {
           <TechListModal />
         </div>
       </>
-    </div>
+    </Provider>
   );
 }
 
