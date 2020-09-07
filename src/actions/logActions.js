00beea62d94049d6d23f369346b6307d98787e66
Thiago Vieira <th91v@outlook.com>
2020-09-07 15:07:23 -0300

    feat: added tech actions and reducer. modified component and rootReducer. modified actions error action

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -40,7 +40,7 @@ export const getLogs = () => async dispatch => {
   } catch (err) {
     dispatch({
       type: LOGS_ERROR,
-      payload: err.response.data
+      payload: err.response.statusText
     })
   }
 }
@@ -69,7 +69,7 @@ export const addLog = (log) => async dispatch => {
   } catch (err) {
     dispatch({
       type: LOGS_ERROR,
-      payload: err.response.data
+      payload: err.response.statusText
     })
   }
 }
@@ -91,7 +91,7 @@ export const deleteLog = (id) => async dispatch => {
   } catch (err) {
     dispatch({
       type: LOGS_ERROR,
-      payload: err.response.data
+      payload: err.response.statusText
     })
   }
 }
@@ -119,7 +119,7 @@ export const updateLog = log => async dispatch => {
   } catch (err) {
     dispatch({
       type: LOGS_ERROR,
-      payload: err.response.data
+      payload: err.response.statusText
     })
   }
 }
@@ -140,7 +140,7 @@ export const searchLogs = (text) => async dispatch => {
   } catch (err) {
     dispatch({
       type: LOGS_ERROR,
-      payload: err.response.data
+      payload: err.response.statusText
     })
   }
 }
--- src/components/techs/TechListModal.js
+++ src/components/techs/TechListModal.js
@@ -1,25 +1,14 @@
 import React, { useState, useEffect } from 'react'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
 import TechItem from './TechItem'
+import { getTechs } from '../../actions/techActions'
 
-const TechListMmodal = () => {
-  const [techs, setTechs] = useState([]);
-  const [loading, setLoading] = useState(false);
+const TechListMmodal = ({ getTechs, tech: { techs, loading } }) => {
 
   useEffect(() => {
     getTechs()
   }, []);
-
-  const getTechs = async () => {
-    setLoading(true);
-
-    const res = await fetch('/techs');
-
-    const data = await res.json();
-
-    setTechs(data);
-
-    setLoading(false);
-  }
 
   return (
     <div id="tech-list-modal" className="modal">
@@ -27,6 +16,7 @@ const TechListMmodal = () => {
         <h4>Technician List</h4>
         <ul className="collection">
           {!loading &&
+            techs !== null &&
             techs.map(tech => (
               <TechItem tech={tech} key={tech.id} />
             ))
@@ -37,4 +27,13 @@ const TechListMmodal = () => {
   )
 }
 
-export default TechListMmodal
+TechListMmodal.propTypes = {
+  tech: PropTypes.object.isRequired,
+  getTechs: PropTypes.func.isRequired,
+}
+
+const mapStateToProps = state => ({
+  tech: state.tech,
+})
+
+export default connect(mapStateToProps, { getTechs })(TechListMmodal)
